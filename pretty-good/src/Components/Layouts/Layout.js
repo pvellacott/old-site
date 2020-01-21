c467065a364483dc1c9c2506dab076872c300161
Phil Vellacott <vellacott.phil@gmail.com>
2020-01-20 23:09:17 -0500

    Some icons with links

--- pretty-good/src/Components/Layouts/Layout.js
+++ pretty-good/src/Components/Layouts/Layout.js
@@ -2,19 +2,38 @@ import React from 'react';
 import Aux from '../../hoc/Aux';
 import ImageAvatar from '../Picture';
 import './Layouts.css';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub, faSpotify, faTwitter } from '@fortawesome/free-brands-svg-icons';
 
+const spaces = {
+    marginRight: '10px',
+    color: 'black',
+}
+
+const iconB = {
+    color: 'black',
+}
+
+
+const WEB_URL = "https://github.com/pvellacott";
+const SPOT_URL = "https://open.spotify.com/user/12174613455"
+const TWIT_URL = "https://twitter.com/phil_vellacott"
 
 const Layout = ( props ) => (    
 <Aux>
-     <div>
-     <FontAwesomeIcon icon="coffee" />
-     </div>
+
         <main>
              {props.children}
-             <ImageAvatar></ImageAvatar>
+                <ImageAvatar></ImageAvatar>
+                <center>
+                    <div className="iconz">
+                            <a href={WEB_URL}><FontAwesomeIcon icon={ faGithub } size="lg" style={ spaces } /></a>
+                            <a href={SPOT_URL}><FontAwesomeIcon icon={ faSpotify } size="lg" style={ spaces } /></a>
+                            <a href={TWIT_URL}><FontAwesomeIcon icon={ faTwitter } size="lg" style={ iconB } /></a>
+                    </div>
+                </center>
              <center>
-             <h1>Hi, I'm Phil.</h1>
+                 <h1>Hi, I'm Phil.</h1>
              </center>
            
         </main>
