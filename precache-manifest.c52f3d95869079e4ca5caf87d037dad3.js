bd90baff8ab6cd369c00ee2b6bd6030ba86ae56c
Phil Vellacott <vellacott.phil@gmail.com>
2020-01-21 22:45:23 -0500

    Updates

---
+++ precache-manifest.c52f3d95869079e4ca5caf87d037dad3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21bc3c6b6d1573c2ae9e91cc3a7295a4",
+    "url": "/pretty-good/index.html"
+  },
+  {
+    "revision": "cdad19a0ea62fc8e8b21",
+    "url": "/pretty-good/static/css/main.0e3e4d3f.chunk.css"
+  },
+  {
+    "revision": "cb50fe9da10ca4c54d67",
+    "url": "/pretty-good/static/js/2.a1956109.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "/pretty-good/static/js/2.a1956109.chunk.js.LICENSE"
+  },
+  {
+    "revision": "cdad19a0ea62fc8e8b21",
+    "url": "/pretty-good/static/js/main.394bffb4.chunk.js"
+  },
+  {
+    "revision": "c383e785333695927338",
+    "url": "/pretty-good/static/js/runtime-main.d669fee1.js"
+  },
+  {
+    "revision": "e4f713b23418ce5555dae4bfa76a1c9e",
+    "url": "/pretty-good/static/media/mustashe.e4f713b2.jpg"
+  }
+]);
