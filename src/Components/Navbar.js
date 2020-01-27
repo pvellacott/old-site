cbba354c541786aaed4d473dba30d9e7d5e247d9
Phil Vellacott <vellacott.phil@gmail.com>
2020-01-26 23:10:24 -0500

    navbar style

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -19,7 +19,40 @@ export default function Navbar() {
     return (
         <div>
         <div style={ style }>
-            <p>About</p>
+            <p css={css`  
+              '&:hover:after' : {
+                width: 100%;
+                background: transparent;
+                transition: all 0s ease;
+              }
+             '&:hover:after': {
+            width: 100%;
+            background: transparent;
+            transition: all 0s ease;
+            }
+            '&:before' : {
+                content: "";
+                display: block;
+                position: absolute;
+                left: 0;
+                bottom: 0;
+                height: 3px;
+                width: 0;
+                transition: width 0s ease, background 0.5s ease;
+              }
+              '&:after' : {
+                content: "";
+                display: block;
+                position: absolute;
+                right: 0;
+                bottom: 0;
+                height: 3px;
+                width: 0;
+                background: #75ebb6;
+                transition: width 0.5s ease;
+              }
+             `}>
+          About</p>
         </div>
         <div style={ style }>
             <p>Projects</p>
